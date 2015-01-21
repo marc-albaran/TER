5141e546fe9b670ef3ccf06e3fed7c6b5303df66
joseph-campomanes <marcjoseph.albaran@gmail.com>
2015-01-21 12:19:26 +0800

    Initial index page

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="_assets\css\bootstrap.min.css">
+<link rel="stylesheet" href="_assets\font-awesome-4.2.0\css\font-awesome.min.css">
+</head>
+
+<body>
+
+	<div class="container">
+		<div id="1">
+		sdfghj
+		</div>
+
+		<div id="2" hidden>
+		sfdghj
+		</div>
+
+		<div id="3" hidden>
+		dfghgjgfdsa
+		</div>
+
+		<div align="center">
+		<nav>
+		  <ul class="pagination">
+			<li>
+			  <a href="#" aria-label="Previous">
+				<i class="fa fa-caret-left"></i>
+			  </a>
+			</li>
+			
+			<li><a href="#1">1</a></li>
+			<li><a href="#2">2</a></li>
+			<li><a href="#3">3</a></li>
+			<li>
+			
+			  <a href="#" aria-label="Next">
+				<i class="fa fa-caret-right"></i>
+			  </a>
+			</li>
+		  </ul>
+		</nav>
+		</div>
+	</div>
+
+</body>
+
+</html>
