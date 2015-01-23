a0d97d3151a51d92bbcd8a7574a0fa4b6e4d0258
joseph-campomanes <marcjoseph.albaran@gmail.com>
2015-01-23 14:42:43 +0800

    changes

--- index.php
+++ index.php
@@ -13,50 +13,75 @@ session_start();
 <head>
 <link rel="stylesheet" href="_assets\css\bootstrap.min.css">
 <link rel="stylesheet" href="_assets\font-awesome-4.2.0\css\font-awesome.min.css">
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-<script type="text/javascript" src="_assets\js\formToWizard.js"></script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $("#SignupForm").formToWizard({ submitButton: 'SaveAccount' })
-    });
-</script>
-<style type="text/css">
-        // body { font-family:Lucida Sans, Arial, Helvetica, Sans-Serif; font-size:13px; margin:20px;}
-        // #main { width:960px; margin: 0px auto; border:solid 1px #b2b3b5; -moz-border-radius:10px; padding:20px; background-color:#f6f6f6;}
-        // #header { text-align:center; border-bottom:solid 1px #b2b3b5; margin: 0 0 20px 0; }
-        // fieldset { border:none; width:320px;}
-        // legend { font-size:18px; margin:0px; padding:10px 0px; color:#b0232a; font-weight:bold;}
-        // label { display:block; margin:15px 0 5px;}
-        // input[type=text], input[type=password] { width:300px; padding:5px; border:solid 1px #000;}
-        // .prev, .next { background-color:#b0232a; padding:5px 10px; color:#fff; text-decoration:none;}
-        // .prev:hover, .next:hover { background-color:#000; text-decoration:none;}
-        // .prev { float:left;}
-        // .next { float:right;}
-        // #steps { list-style:none; width:100%; overflow:hidden; margin:0px; padding:0px;}
-        // #steps li {font-size:24px; float:left; padding:10px; color:#b0b1b3;}
-        // #steps li span {font-size:11px; display:block;}
-        // #steps li.current { color:#000;}
-        // #makeWizard { background-color:#b0232a; color:#fff; padding:5px 10px; text-decoration:none; font-size:18px;}
-        // #makeWizard:hover { background-color:#000;}
-    </style>
 </head>
 
 <body>
 
 		<div class="container">
+		<br>
+		<div class="row">
+		
 		<div class="col-md-2"></div>
-		<div class="row col-md-8">
-				<div id="details" style="background-color: #6495ed;">
-					<strong>Name of Faculty:</strong>
-					<br><strong>Subject:</strong>
-					<br><strong>Section:</strong>
-					<br><strong>Schedule:</strong>
+		<div class="row col-md-8" style="background-color: #d3ffd3;">
+				<!---->
+				<div id="details" class="row" style="background-color: #d3d3d3;">
+					<div class="col-md-3" align="center" style="background-color: #d3d3ff;">
+						<br>
+						<img src="_assets\img\alt.jpg" alt="" class="img-responsive img-circle img-thumbnail" width="60%">					
+						<br>
+						<strong>Alquine Roy F. Taculin</strong>
+						<p class="text-muted">CSc 100<br>IT1.B</p>
+					</div>
+					
+					<div class="col-md-9" align="left">
+						TER cover photo here
+					</div>
+				<br>	
 				</div>
-				<div id="main" background-color: #6495es;>
-				content here
+				<br>
+				<div id="main">
+					
+					<p>Evaluate your teacher objectively based on the given criteria.</p>
+					
+					<form method="post" action="#">
+					
+					<h4>I.</h4>
+					<p>1. Attends class regularly.</p>
+					<p>2. Comes to class on time.</p>
+					<p>3. Give reasons when class is suspended.</p>
+					<p>4. Dissmisses class on time.</p>
+
+					<h4>II.</h4>
+					<p>5. Comes to class well prepared.</p>
+					<p>6. Answers well the question of the students.</p>
+					<p>7. Presents lessons in an orderly manner.</p>
+					<p>8. Presents lessons clearly with present and future application/use.</p>
+					<p>9. Covers satisfactorily the contents of the course.</p>
+
+					<h4>III.</h4>
+					<p>10. Gives illustrations, examples that makes lessons clear.</p>
+					<p>11. Gives and returns quizzes/exams regularly after correcting.</p>
+					<p>12. Encourage active participation of students.</p>
+					<p>13. Listens attentively to student&#39;s reports disscussions in class.</p>
+					<p>14. Uses different methods and techniques of teaching to make lessons clear.</p>
+					<p>15. Varies teaching strategies to make lessons interesting.</p>
+					<p>16. Motivates students to thik and analyze.</p>
+					<p>17. Shows approachability and open mindedness.</p>
+
+					<h4>IV.</h4>
+					<p>18. I learn from this teacher.</p>
+					<p>19. The teacher is fair in giving grades.</p>
+					<p>20. I will advise my friends to be &#34;under&#34; this teacher.</p>
+					
+					<button type="submit" class="btn btn-primary"><i class="fa fa-share"></i> Submit</button>
+					
+					</form>
 				</div>
 		</div>
+		
 		<div class="col-md-2"></div>
+		</div>
+		
 		</div>
 		
 		
