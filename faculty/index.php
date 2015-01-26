1c9fca2fbb1f875e1b535f6a56b25b0ca07e0230
joseph-campomanes <marcjoseph.albaran@gmail.com>
2015-01-26 16:06:44 +0800

    new file structure

---
+++ faculty/index.php
@@ -0,0 +1,20 @@
+<div class="form-group">
+					   <label class="col-xs-2 control-label">Date Range</label>
+					   <div class="col-xs-9">
+						<div class="radio">
+						 <label>
+						  <input id="inlineradio1" name="sampleinlineradio" value="option1" type="radio">
+						  Radio 0</label>
+						</div>
+						<div class="radio">
+						 <label>
+						  <input id="inlineradio1" name="sampleinlineradio" value="option1" type="radio">
+						  Radio 0</label>
+						</div>
+						<div class="radio">
+						 <label>
+						  <input id="inlineradio1" name="sampleinlineradio" value="option1" type="radio">
+						  Radio 0</label>
+						</div>
+					   </div>
+					  </div>
